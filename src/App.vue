d3d24098c75727e1cce7976eeeb2aa43409ec56c
hershellayton95 <filidm5795@gmail.com>
2023-03-08 14:38:03 +0100

    update v-model

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,42 @@ export default {
   </div>
   <p>{{ value }}</p>
 </template>
+
+<!-- <script>
+import Button from "./components/Button.vue";
+import Input from "./components/Input.vue";
+export default {
+  components: {
+    Button,
+    Input,
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    setValue(input) {
+      this.value = input;
+    },
+    handleClick() {
+      console.log(this.value);
+    },
+    handleChange(e) {
+      this.setValue(e.target.value);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Input
+      type="text"
+      placeholder="inserisci un nome"
+      v-bind:value="value"
+      :onChange="handleChange" />
+    <Button :onClick="handleClick">Button</Button>
+  </div>
+  <p>{{ value }}</p>
+</template> -->
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -26,3 +26,25 @@ export default {
     :value="modelValue"
     @input="$event => $emit('update:modelValue', $event.target.value)" />
 </template>
+
+<!-- <script>
+export default {
+  props: {
+    type: String,
+    value: String,
+    placeholder: String,
+    onChange: {
+      type: Function,
+    },
+  },
+};
+</script>
+/* v-on:input == @input e v-bind:type === :type */
+<template>
+  <input
+    class="inline-block my-2 mx-1 py-[0.35rem] px-2 leading-6 align-middle rounded-sm outline outline-2 focus:outline-offset-1 outline-gray-400 placeholder:italic"
+    :type="type"
+    :value="value"
+    :placeholder="placeholder"
+    @input="onChange" />
+</template> -->
